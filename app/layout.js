891ced78f08acc53dfa15edc68034a9ffc9f4560
Reginald Simon <96483953+Regie4233@users.noreply.github.com>
2023-07-23 21:59:23 -0400

    nit

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Inter, Roboto } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Roboto({ subsets: ['latin'], weight:'400' })
 
 export const metadata = {
   title: 'Create Next App',
--- app/page.js
+++ app/page.js
@@ -1,113 +1,67 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import { Satisfy, Truculenta } from 'next/font/google';
+const topfont = Satisfy({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: '400'
+})
+const subfont = Truculenta({
+  subsets: ['latin'],
+  weight: '400'
+})
 
 export default function Home() {
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className='border-y-8 border-yellow-500 h-full'>
+      <section className='flex flex-col my-10 lg:my-20 mx-5 md:mx-24'>
+        <div className={`text-5xl md:text-6xl lg:text-9xl ${topfont.className} flex flex-row gap-4 md:gap-10 text-amber-950 justify-center`}>
+          Melted
+          <div className='relative w-[50px] md:w-[70px] lg:w-[150px]'>
+            <Image alt='main logo deco' src={'/deco/meltedbutter.png'} priority fill />
+          </div>
+          Butter
         </div>
-      </div>
+        <section className={`${subfont.className} h-[150px] px-2 lg:px-10 p-4 text-yellow-50 bg-yellow-500 my-5 flex flex-col`}>
+          <h1 className='text-center text-md md:text-2xl lg:text-4xl'>Step into a world of pure delight and indulge your senses.
+            I take pride in sourcing only the finest, freshest ingredients, ensuring that my creations are of the highest quality
+          </h1>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+         
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        </section>
+        <nav className='flex flex-row text-xl md:text-4xl lg:text-7xl gap-10 justify-center bg-yellow-600 text-yellow-50 py-5'>
+            <a href='' className='border-b-2 border-dashed hover:scale-110 transform transition duration-500'>Cakes</a>
+            <a href='' className='border-b-2 border-dashed hover:scale-110 transform transition duration-500'>Cupcakes</a>
+            <a href='' className='border-b-2 border-dashed hover:scale-110 transform transition duration-500'>Pastries</a>
+            <a href='' className='border-b-2 border-dashed hover:scale-110 transform transition duration-500'>Pies</a>
+          </nav>
+        <section className='flex flex-row justify-between my-16'>
+          <div className='p-4 bg-yellow-500 font-bold text-yellow-50 text-xl w-[300px] '>
+            <p className='border-b-2 border-dashed'>View Full Menu</p>
+          </div>
+          <div className='text-xl text-end flex flex-col'>
+            <p>2999 N. Broadway ave, Englemall, NY 69690</p>
+            <a>212-989-1099</a>
+            <a>meltedbutter@bakery.com</a>
+          </div>
+        </section>
+        <section>
+          SOME TEXT
+        </section>
+        <section className='flex flex-row justify-between my-32'>
+          <div className='lg:w-[350px] lg:h-[350px] relative'>
+            <Image alt='featured pic 1' fill src={'/images/bluehalfcake.png'}/>
+          </div>
+          <div>
+            DECO
+          </div>
+          <div className='lg:w-[350px] lg:h-[350px] relative'>
+            <Image alt='featured pic 1' fill src={'/images/blueberry2.png'}/>
+          </div>
+        </section>
+      </section>
     </main>
   )
 }
